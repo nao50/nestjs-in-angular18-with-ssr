97743bd526ce4a483c5c14e662d66a0545fb348b
naoyamaguchi <naoyamaguchi0128@gmail.com>
2024-07-28 14:39:51 +0900

    Add angular frontend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
+// 
+import { APP_BASE_HREF } from '@angular/common';
+import { CommonEngine } from '@angular/ssr';
+import bootstrap from './angular-frontend/src/main.server';
+import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 @Controller()
 export class AppController {
+  commonEngine = new CommonEngine();
+
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('')
+  frontend(@Req() req: Request): any {
+    const serverDistFolder = dirname(fileURLToPath('./angular-frontend/dist/angular-frontend/server'));
+    const browserDistFolder = resolve(serverDistFolder, '../browser');
+    const indexHtml = join(serverDistFolder, 'index.server.html');
+
+    const { protocol, originalUrl, baseUrl, headers } = req;
+  
+    this.commonEngine.render({
+      bootstrap,
+      documentFilePath: indexHtml,
+      url: `${protocol}://${headers.host}${originalUrl}`,
+      publicPath: browserDistFolder,
+      providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+    });
+  }
+
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { CommonEngine } from '@angular/ssr';
+// 
+import { fileURLToPath } from 'node:url';
+import { dirname, join, resolve } from 'node:path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('view engine', 'html');
+  app.set('views', './angular-frontend/dist/angular-frontend/browser');
+
   await app.listen(3000);
 }
 bootstrap();
